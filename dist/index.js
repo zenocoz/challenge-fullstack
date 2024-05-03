19180d1e9a75c661aec985ed74edfc0a92ecb536
zenocoz <federico.soncini@gmail.com>
2024-05-03 12:37:49 +0200

    exports app module

--- dist/index.js
+++ dist/index.js
@@ -18,4 +18,5 @@ setInterval(utilities_1.fetchPrices, 60000); // 60000 milliseconds = 60 seconds
 app.listen(port, () => {
     console.log(`Server is running at port:${port}`);
 });
+module.exports = app;
 //# sourceMappingURL=index.js.map
